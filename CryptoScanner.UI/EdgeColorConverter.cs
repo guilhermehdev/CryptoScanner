0763d43ed69cddcb48dcd331ad1aebf452290a26
Guilherme <68760390+guilhermehdev@users.noreply.github.com>
2026-08-28 13:13:14 -0300

    fix: restore EdgeColorConverter for WPF XAML

---
+++ CryptoScanner.UI/EdgeColorConverter.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Globalization;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace CryptoScanner.UI;
+
+public sealed class EdgeColorConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        return value switch
+        {
+            double d => d >= 0 ? Brushes.DarkGreen : Brushes.DarkRed,
+            decimal d => d >= 0 ? Brushes.DarkGreen : Brushes.DarkRed,
+            float d => d >= 0 ? Brushes.DarkGreen : Brushes.DarkRed,
+            int d => d >= 0 ? Brushes.DarkGreen : Brushes.DarkRed,
+            long d => d >= 0 ? Brushes.DarkGreen : Brushes.DarkRed,
+            _ => Brushes.Gray
+        };
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        => throw new NotSupportedException();
+}
